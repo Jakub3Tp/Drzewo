e13784310aecb776ff666e76f9cfb7d97d74d384
student06 <jakub.gola2@teb.edu.pl>
2025-01-17 11:24:33 +0100

    17.01.2025

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+from Tree import Tree
+from TreeNode import TreeNode
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+tree = Tree(10)
+node1 = TreeNode(5, None, None, tree.root)
+tree.add_node(node1, tree.root)
+node2 = TreeNode(2,None, None, tree.root)
+tree.add_node(node2, tree.root)
+node3 = TreeNode(7, None, None, tree.root)
+tree.add_node(node3, node1)
+node4 = TreeNode(6, None, None, tree.root)
+tree.add_node(node4, node1)
+node5 = TreeNode(3, None, None, tree.root)
+tree.add_node(node5, node2)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+tree.dfs_show(tree.root)
